1bb04d5d983cac494c38d6f3b1fc1e6149e6d0ee
Andrew Dickinson <dickinsona19@yahoo.com>
2025-04-25 09:15:50 -0400

    Added some services

--- src/main/java/com/BossLiftingClub/BossLifting/User/UserService.java
+++ src/main/java/com/BossLiftingClub/BossLifting/User/UserService.java
@@ -18,9 +18,9 @@ public interface UserService {
     User signInWithPhoneNumber(String phoneNumber, String password) throws Exception;
     Optional<User> updateProfilePicture(Long id, String profilePicture);
     User getUserByReferralCode(String referralCode);
-
     User updateUser(User user);
     boolean existsByReferralCode(String referralCode);
     Optional<User> updateWaiverSignature(Long userId, String imageUrl);
     boolean updateReferralCode(String referralCode, String newReferralCode);
+    User updateUserOver18(long userId);
 }
