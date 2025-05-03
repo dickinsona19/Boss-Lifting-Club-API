95fde11c7b6b44e77cef9af15e3a04a97ca645c3
Andrew Dickinson <dickinsona19@yahoo.com>
2025-05-03 12:12:50 -0400

    Added some services

--- src/main/java/com/BossLiftingClub/BossLifting/User/PasswordAuth/JwtUtil.java
+++ src/main/java/com/BossLiftingClub/BossLifting/User/PasswordAuth/JwtUtil.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Component
 public class JwtUtil {
     private String secret = "X7kP9mN2vQ8rT5wY1zA3bC6dE9fG2hJ4kL5mN6pQ8rS9tU0vW1xY2zA3B4C5D6E7F8G9H0I1J2K3L4M5N6O7P8Q9"; // Replace with a strong secret (store in env vars in production)
-    private long expiration = 1000 * 60 * 60 * 10; // 10 hours in milliseconds
+    private long expiration = 1000L * 60 * 60 * 24 * 365*5;// 10 hours in milliseconds
 
     // Generate token (used during login, included for context)
     public String generateToken(String username) {
